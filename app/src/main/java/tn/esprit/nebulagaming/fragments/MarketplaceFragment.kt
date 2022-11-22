36006d2b2469d84456fa91235548f37ad784d1ef
Aymen Dhahri <41507665+DroidZed@users.noreply.github.com>
2022-11-22 11:04:25 +0100

    viewpager doesn't work

--- app/src/main/java/tn/esprit/nebulagaming/fragments/MarketplaceFragment.kt
+++ app/src/main/java/tn/esprit/nebulagaming/fragments/MarketplaceFragment.kt
@@ -1,14 +1,20 @@
 package tn.esprit.nebulagaming.fragments
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
 import tn.esprit.nebulagaming.R
 
 
 class MarketplaceFragment : Fragment() {
+
+    private lateinit var tabLayout: TabLayout
+    private lateinit var viewPager: ViewPager2
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,5 +27,13 @@ class MarketplaceFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_marketplace, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        tabLayout = view.findViewById(R.id.tabLayout)
+        viewPager = view.findViewById(R.id.mpViewPager)
+        TabLayoutMediator(tabLayout, viewPager) { _, _ -> }.attach()
+
     }
 }
